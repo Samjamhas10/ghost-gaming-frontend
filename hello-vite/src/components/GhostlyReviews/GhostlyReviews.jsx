5d18e22f750fa1a67a178d4a424daeee8f053ceb
SamSam Hashi <sammymadobe@gmail.com>
2025-07-18 17:57:06 -0500

    Updated code

--- hello-vite/src/components/GhostlyReviews/GhostlyReviews.jsx
+++ hello-vite/src/components/GhostlyReviews/GhostlyReviews.jsx
@@ -3,10 +3,15 @@ import { Reviews } from "../../utils/constants.js";
 import "./GhostlyReviews.css";
 
 function GhostlyReviews() {
+  console.log(Reviews)
   return (
     <div className="ghostly__reviews">
       <h1 className="reviews__title">Ghostly Reviews:</h1>
       <div className="reviews__image-1"></div>
+      {/* Add the review styling and key*/}
+      {Reviews.map((review) => (
+        <div> review.id</div>
+      ))}
       <p className="reviews__text">{Reviews.review}</p>
       <div></div>
     </div>
--- hello-vite/src/utils/constants.js
+++ hello-vite/src/utils/constants.js
@@ -1,9 +1,15 @@
-export const Reviews = {
-  id: 1,
-  key: 1,
-  name: "Slayer",
-  review: "This is the best game ever!",
-  date: "05/10/2020",
-};
+export const Reviews = [
+  {
+    id: 1,
+    key: 1,
+    name: "Slayer",
+    review: "This is the best game ever!",
+    date: "05/10/2020",
+  },
+];
+
+// add game id 
+
+// Mock review data types
 
 export const APIkey = "";

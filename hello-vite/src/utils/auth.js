d28d16c8c7c97b75e016c35c46fad8e110885bfd
SamSam Hashi <sammymadobe@gmail.com>
2025-07-30 19:26:49 -0500

    WIP: remove hardcoded data in auth and start implementing profile route

--- hello-vite/src/utils/auth.js
+++ hello-vite/src/utils/auth.js
@@ -7,9 +7,9 @@ export const authorize = (email, password) => {
 export const checkToken = (token) => {
   return new Promise((resolve, reject) => {
     resolve({
-      name: "Joseph",
-      avatarUrl:
-        "https://plus.unsplash.com/premium_photo-1750116257648-64c9c39dbd8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      // name: "Joseph",
+      // avatarUrl:
+      //   "https://plus.unsplash.com/premium_photo-1750116257648-64c9c39dbd8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     });
   });
 };

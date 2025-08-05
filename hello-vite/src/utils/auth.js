f843ce1a6ffb1a1ef2d5a9ce6a2712c8cca82988
SamSam Hashi <sammymadobe@gmail.com>
2025-08-05 10:53:09 -0500

    Fix: implement signup and signin with backend url

--- hello-vite/src/utils/auth.js
+++ hello-vite/src/utils/auth.js
@@ -1,21 +1,33 @@
+const BASE_URL = "http://localhost:3004";
+
+import { checkResponse } from "./IGDBApi";
+
 export const authorize = (email, password) => {
-  return new Promise((resolve, reject) => {
-    resolve({});
-  });
+  return fetch(`${BASE_URL}/users/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
 };
 
 export const checkToken = (token) => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      // name: "Joseph",
-      // avatarUrl:
-      //   "https://plus.unsplash.com/premium_photo-1750116257648-64c9c39dbd8d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    });
-  });
+  return fetch(`${BASE_URL}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
 };
 
-export const register = (email, password, name, avatar) => {
-  return new Promise((resolve, reject) => {
-    resolve({});
-  });
+export const register = (email, password, name, avatarUrl) => {
+  return fetch(`${BASE_URL}/users/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name, avatarUrl }),
+  }).then(checkResponse);
 };
